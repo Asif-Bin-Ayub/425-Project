cc824dc70200d4368faa3a6c19ef56e4d3b9d070
Asif Bin Ayub <numberphile08@gmail.com>
2021-11-22 09:01:24 +0300

    Working RK-4 method without functions

--- SimplePendulum/Asif/Asif_SimplePendulum.f90
+++ SimplePendulum/Asif/Asif_SimplePendulum.f90
@@ -1,70 +1,73 @@
-!Program to simulate a non-linear simple pendulum using RK-4
-!Uses procedures to make the program modular
+! Program to simulate a non-linear simple pendulum using RK-4
+! Uses procedures to make the program modular
 
 program SimplePendulum
 implicit none
 	
-	!Variable Declarations
+	! Variable Declarations
 		
-	!Constants
-	!g = accleration due to gravity (m/s^2), l = length of pendulum
+	! Constants
+	! g = accleration due to gravity (m/s^2), l = length of pendulum
 	double precision, parameter :: g = 9.8d0, l = 9.8d0, pi = acos(-1.00)
 	
-	!Parameters of motion
-	!theta = angular displacement (rad), omega = angular velocity (rad/s) 
-	!K = RK-4 derivative, ki = RK-4 coefficients
+	! Parameters of motion
+	! theta = angular displacement (rad), omega = angular velocity (rad/s) 
+	! K = RK-4 derivative, ki = RK-4 coefficients
 	double precision :: theta, omega, x, y, t, dt
 	double precision :: k1_th, k2_th, k3_th, k4_th, K_th
 	double precision :: k1_w, k2_w, k3_w, k4_w, K_w
 	
-	!Initializations
+	! Initializations
 	dt = 1d-2 !sec
 	t = 0d0 !sec
 
-	!Initial conditions
+	! Initial conditions
 	theta = 0.5d0 !rad
 	omega = 0d0 !rad/s
 	
-	!Initial Positions
+	! Initial Positions
 	x = l*sin(theta)
 	y = l*(1-cos(theta))
-		
+	
+	! Result files
 	open(11, file = "Pendulum_RK4.txt")
-	open(22, file = "Trajectory_RK4.txt")
 		
 	!RK-4 loop
 	do while (t <= 60d0)
 		
 		!Writing results into file
-		write(11,*) t, theta, omega
-		write(22,*) t, x, y
+		write(11,*) t, theta, omega, x, y
 		
 		
-		!k1
+		! k1
 		k1_th = omega
-		k1_w = A(theta, 0d0)
+		! k1_w = A(theta, 0d0)
+		k1_w = -(g/l)*sin(theta)
 		
-		!k2
+		! k2
 		k2_th = omega + k1_w*(dt/2d0)
-		k2_w = A(theta, k1_th)
+		! k2_w = A(theta, 0.5d0*k1_th) ! Factor of 1/2 for k2 & k3 coefficients
+		k2_w = -(g/l)*sin(theta + k1_th*(dt/2d0))
 		
-		!k3
+		! k3
 		k3_th = omega + k2_w*(dt/2d0)
-		k3_w = A(theta, k2_th)
+		! k3_w = A(theta, 0.5d0*k2_th) ! Factor of 1/2 for k2 & k3 coefficients
+		k3_w = -(g/l)*sin(theta + k2_th*(dt/2d0))
 		
-		!k4
+		! k4
 		k4_th = omega + k3_w*dt
-		k4_w = A(theta, 2d0*k3_th) !Factor of 2 for dt in k4
+		! k4_w = A(theta, k3_th)
+		k4_w = -(g/l)*sin(theta + k3_th*dt)
 		
-		!Derivatives
+		! Derivatives
 		K_th = K(k1_th, k2_th, k3_th, k4_th)
 		K_w =  K(k1_w, k2_w, k3_w, k4_w)
 		
-		!Updating parameters
+		! Updating parameters
 		theta = theta + K_th*dt
 		omega = omega + K_w*dt
 		
-		!Updating position
+		! Updating positions
 		x = l*sin(theta)
 		y = l*(1-cos(theta))
 		
@@ -73,14 +76,14 @@ implicit none
 		end do
 	
 	close(11)
-	close(22)
+	
 	
 	contains
 	
-	!Function for angular acceleration alpha
+	! Function to evaluate angular acceleration (alpha)
 	function A(theta, k)
 	implicit none
-		!Dummy arguments
+		! Dummy arguments
 		double precision :: A, theta, k, dt
 		double precision :: g, l
 		
@@ -89,16 +92,17 @@ implicit none
 		
 		dt = 1d-2 !sec
 		
-		theta = theta + k*(dt/2d0)
+		theta = theta + k*dt
 		
 		A = -(g/l)*sin(theta)
 
 	end function A
+	
 
-	!Function to calculate derivatives for RK-4
+	! Function to calculate derivatives for RK-4
 	function K(k1, k2, k3, k4)
 	implicit none
-		!Dummy arguments
+		! Dummy arguments
 		double precision :: K, k1, k2, k3, k4
 		
 		K = (k1 + 2*k2 + 2*k3 + k4)/6d0
