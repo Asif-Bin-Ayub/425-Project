b162daee3066d26961025fe295378dfddc7be1c5
Asif Bin Ayub <numberphile08@gmail.com>
2021-12-02 15:29:08 +0300

    Added Damping & Driving Forces

--- Part I/Project_Part1.f90
+++ Part I/Project_Part1.f90
@@ -9,7 +9,6 @@ double precision :: theta,der_theta,omega,der_omega,K,AV,AA
 
 !Parameters:===========================
 pi=acos(-1.0d0)
-g=9.8d0 !m/s^2
 l=9.8d0 !m
 dt=0.01d0
 tmax=60.0d0 !s
@@ -30,31 +29,39 @@ z=-l*(1+cos(theta))+2.0d0*l !m
 !Storing results:======================
 !Path1 (Parabola):---------------------
 	!Trajectory:
-	! open(unit=10,file='Trajectory_Path1.txt')
+	open(unit=10,file='Trajectory_Path1.txt')
 	!Equilibrium Point(s):
-	! open(unit=11,file='Equilibrium.txt')
+	! open(unit=11,file='Equilibrium_Path1.txt')
 	
 !Path2 (Unknown):----------------------
 	!Trajectory:
-	open(unit=12,file='Trajectory_Path2.txt')
+	! open(unit=12,file='Trajectory_Path2.txt')
 	
 !Loop:================================== 
 do while(t.le.tmax)
+	
+	!Writing Results:
+	!Path1:------------
+		write(10,*) t,theta,omega,x,z
+		! if(AA(theta,omega).le.3.0d-4 .and. AA(theta,omega).ge.-3.0d-4) write(11,*) t !To determine equlibrium poiunts
+	!Path2:----------------
+		! write(12,*) t,theta,omega,x,z
+	
 	!K1
 	th_k1= AV(theta,omega) 
-	w_k1= AA(theta,omega)
+	w_k1= AA(theta,omega,t)
 	
 	!K2
 	th_k2= AV(theta + 0.5d0*th_k1*dt,omega + 0.5d0*w_k1*dt)
-	w_k2= AA(theta + 0.5d0*th_k1*dt,omega + 0.5d0*w_k1*dt)
+	w_k2= AA(theta + 0.5d0*th_k1*dt,omega + 0.5d0*w_k1*dt,t)
 	
 	!K3
 	th_k3= AV(theta + 0.5d0*th_k2*dt,omega + 0.5d0*w_k2*dt)
-	w_k3= AA(theta + 0.5d0*th_k2*dt,omega + 0.5d0*w_k2*dt)
+	w_k3= AA(theta + 0.5d0*th_k2*dt,omega + 0.5d0*w_k2*dt,t)
 	
 	!K4
 	th_k4= AV(theta + th_k3*dt,omega + w_k3*dt)
-	w_k4= AA(theta + th_k3*dt,omega + w_k3*dt)
+	w_k4= AA(theta + th_k3*dt,omega + w_k3*dt,t)
 	
 	!Rk4 Derivatives 
 	der_theta=K(th_k1,th_k2,th_k3,th_k4)
@@ -65,22 +72,21 @@ do while(t.le.tmax)
 	
 	!Trajectory:
 	!Path1:------------
-		! x=l*sin(theta)/(1.0d0+cos(theta))
-		! z=-l*cos(theta)/(1.0d0+cos(theta))
+		x=l*sin(theta)/(1.0d0+cos(theta))
+		z=-l*cos(theta)/(1.0d0+cos(theta))
 	!Path2:------------
-		x=l*(theta+sin(theta))
-		z=-l*(1+cos(theta))+2.0d0*l
+		! x=l*(theta+sin(theta))
+		! z=-l*(1+cos(theta))+2.0d0*l
 	
 	!Time Update:
 	t=t+dt
 	
-	!Writing Results:
-	!Path1:------------
-		! write(10,*) t,theta,omega,x,z
-		! if(AA(theta,omega).le.3.0d-4 .and. AA(theta,omega).ge.-3.0d-4) write(11,*) t !To determine equlibrium poiunts
-	!Path2:----------------
-		write(12,*) t,theta,omega,x,z
 enddo
+
+	! Closing results files
+	close(10)
+	close(11)
+	close(12)
 
 end program Preject_Part1
 
@@ -95,15 +101,36 @@ end program Preject_Part1
 	end function K
 	
 	! Angular Acceleration (AA):
-	function AA(theta,omega)
-		double precision :: g,l,theta,omega
+	! Take Fd=0 & q=0 UNLESS you want to simulate damping and driving forces 
+	function AA(theta,omega,t)
+		double precision :: g,l,theta,omega,t
+		double precision :: q,Fd,omega_d
 		double precision :: AA
 		g=9.8d0
 		l=9.8d0
-		!Path1:-----------
-			! AA = -0.5d0*g/l*sin(theta)*(1.0d0+cos(theta))-1.5d0*omega**2*sin(theta)/(1.0d0+cos(theta))
-		!Path2:----------
-			AA = 0.5d0*sin(theta)*(omega**2-g/l)/(1.0d0+cos(theta))
+	
+		q=0.3d0 ! Damping coefficient
+		Fd=12d0 ! Driving amplitude
+		omega_d=2*acos(-1.0)/1d0 ! Driving frequency
+		
+		! Notes on damping coefficient q (with Fd=0)
+		! Path 1:
+			! 2 or more --> Overdamped (2 is not the critical damping coefficient, just roughly)
+			! less than 2 --> Underdamped
+			! less than 1 --> Sizeable oscillations
+		! Path 2:
+			! 2 or more --> Overdamped (2 is not the critical damping coefficient, just roughly)
+			! less than 2 --> Underdamped
+			! less than 0.5 --> Sizeable oscillations
+			
+		! Notes on Driving amplitude Fd & omega_d
+			! For path 2, the solution blows up for Fd more than 1.2 (Need confirmation)
+			! For path 1, the solution blows up for Fd more than 11 (Need confirmation)
+			
+		! Path1:-----------
+			AA = -0.5d0*g/l*sin(theta)*(1.0d0+cos(theta))-1.5d0*omega**2*sin(theta)/(1.0d0+cos(theta))-q*omega+Fd*sin(omega_d*t)
+		! Path2:----------
+			! AA = 0.5d0*sin(theta)*(omega**2-g/l)/(1.0d0+cos(theta))-q*omega+Fd*sin(omega_d*t)
 	end function AA
 		
 	! Angular Velocity (AV):
